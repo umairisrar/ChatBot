c632403005da9bdb7d60fa9f5f46884e4cd3bceb
umairisrar <umair_israr92@hotmail.com>
2023-12-07 14:56:31 +0500

    Updated

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -11,19 +11,11 @@ const Header = async ({ username }) => {
       {userId ? (
         <div className="h-[100%] w-[100%] flex justify-center items-center">
           <div className="flex items-center flex-row gap-[10px]">
-            {/* <Link href="profile">
-              <button className="text-[white] text-[21px] border-[1px] border-[white] rounded-[5px] p-[6px] bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
-                Profile
-              </button>
-            </Link> */}
             <Link href="chatbot">
               <button className="text-[white] text-[21px] border-[1px] border-[white] rounded-[5px] p-[6px] bg-[#1d4ed8] hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
                 Chatbot
               </button>
             </Link>
-            {/* <div className="ml-auto">
-              <UserButton afterSignOutUrl="/" />
-            </div> */}
           </div>
         </div>
       ) : (
